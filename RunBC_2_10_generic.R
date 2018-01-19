d32e1893e81776da6770f7b7bc1fc8ad310e9320
mttinker <ttinker@nhydra.com>
2018-01-19 17:44:17 -0400

    BayesCalls 2.10 initial

--- RunBC_2_10_generic.R
+++ RunBC_2_10_generic.R
@@ -21,14 +21,14 @@ start.time <- Sys.time()  # start a timer
 # First provide a "Root Directory" within which all other folders, subfolders & files are contained
 #   NOTE: enter "current" for current folder, "above1" for 1 folder above, "above2" for 2 above, 
 #   otherwise enter path with "/" separator for sub-folders
-RootDir =  "D:/CM,Inc/Dropbox (CMI)/CMI_Team/Analysis/2017/Bayesian_2017/CapCays_WTSHBLNO"  # Examples "current" or "above1" or "C:/Users/XXXX/BayesianMethods"
-AnalysisFolder = 'BayesCalls2_02'  # Folder path within RootDir where analysis code is stored
-RunFile = 'BayesCalls2_02'       # Version of BayesCalls to run
-DataFolder =  'data'  # Folder path within RootDir where raw data files stored
+RootDir =  "above1"  # Examples "current" or "above1" or "C:/Users/XXXX/BayesianMethods"
+AnalysisFolder = 'Acoustic2'  # Folder path within RootDir where analysis code is stored
+RunFile = 'BayesCalls2_10'       # Version of BayesCalls to run
+DataFolder =  'CapCays/data'  # Folder path within RootDir where raw data files stored
 ProjectName =  'Bayesian_2017'  # Name of project (for user reference)
-Species =  ''  # Species name for data analysis
+Species =  'WTSH'  # Species name for data analysis
 Countsdatfile = c(paste0('Counts_CapCays_',Species,'_2014-2017.csv')) # Name of matching data file with nest count data (OTIONAL, enter blank if no nest counts)
-Datafile =  paste0('BayesCalls2_02_',Species,'_2014-2017.RData')  #Name of data file for analysis 
+Datafile =  paste0('BayesCalls2_02_',Species,'_2014-2017_NoAudit.RData')  #Name of data file for analysis 
 ProjectLocation =  'Queensland'  # Island name for data analysis
 ProjectYear =  2017  # Year of analysis project 
 Yearfocal =  2017  # Focal year for Bayesian analysis
@@ -37,7 +37,7 @@ TS_strt =  1  # Starting time step (beginning of range to load data)
 TS_stop =  57  # Ending timestep (end of range to load data)
 TimeStep1 =  '5:00PM'  # Clock time (24hr) of timestep1
 TimeStepIntvl =  15  # Number of minutes in each timestep
-subsamp =  3  # Level of Sub-sampling of entire data set: use every nth record
+subsamp =  10  # Level of Sub-sampling of entire data set: use every nth record
 
 if (Yearfocal==2017) {
     # Analysis period
@@ -56,7 +56,7 @@ if (Yearfocal==2017) {
 # set peak dates (DOY not day of survey)
 if (Species=='WTSH') {
   peakdates_strt = 50
-  if (Yearfocal==2016) { peakdates_end = 110 } else { peakdates_end = 109 }
+  if (Yearfocal==2016) { peakdates_end = 109 } else { peakdates_end = 109 }
 } else if (Species=='BLNO') {
   # still unclear when noddies are most active
   peakdates_strt = 1
@@ -85,12 +85,12 @@ if (Species=='WTSH') {
 # peaktimes_stop2 =  120  # OPTIONAL: if peaktimes_ref = 3, this is second Peak time boundary 2 (always > boundary1) 
 
 calendar_opt =  2  # year range: 1 = All data within calendar year, 2 = data spans New Year
-data_opt =  2  # Data Option: 1 = Calls Only, 2 = Calls plus Nest Counts
-prior_opt =    # Priors: 1 = uninformed, 2 = informed (must supply results file)
+data_opt =  1  # Data Option: 1 = Calls Only, 2 = Calls plus Nest Counts
+prior_opt = 1   # Priors: 1 = uninformed, 2 = informed (must supply results file)
 PriorResultsFile =  c()  # OPTIONAL: if prior_opt = 2, Rdata file containing parameter priors
 
-Ncores = 8
-Nsim =  2750  # Total # MCMS sims: Actual saved reps = (Nsim-Nburnin) * (num Cores)
+Ncores = 20
+Nsim =  2000  # Total # MCMS sims: Actual saved reps = (Nsim-Nburnin) * (num Cores)
 Nburnin =  1500  # Number of burn-in reps: Actual reps = (Nsim-Nburnin) * (num Cores)
 Nadapt =  100  # Number of adapting reps, default 100
 
